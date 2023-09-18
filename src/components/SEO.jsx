f1f45b27a9fe63c703e66fec8c70beca39f27ecb
Sebastian Septien <sebsepdus@hotmail.com>
2023-09-17 20:49:56 -0600

    Indexing the website of the URL

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function SEO() {
   return (
     <>
+
         <meta  />
+	<meta name="google-site-verification" content="38UvtEOkIvDZnNnLzJSdPosB8MMJwKv1LFHGAnjDBP0" />
     </>
   )
 }
